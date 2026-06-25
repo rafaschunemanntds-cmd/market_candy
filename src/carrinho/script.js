827b290484f2695a6796ec8aa76eab535e664a2b
gwenzinha <gwenbonequinhalindinha@gmail.com>
2026-06-25 09:30:47 -0300

    Add files via upload

---
+++ src/carrinho/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    const carrinho = JSON.parse(localStorage.getItem("carrinho")) || []
+    const listElement = $('#lista')
+    const totalElement = $("#total")
+
+    function exibirCarrinho(){
+        listElement.empty()
+        let totalPreco = 0
+
+        $.each(carrinho, function(index, item){
+            const listItem = $("<li>").text(`${item.desc} - Preço: $${item.preco.toFixed(2)}`)
+
+            const removeButton = $("<button>").text("❌").css("margin-left", "10px").click(function(){
+                removerItem(index)
+            })
+            
+        })
+    }
+
+
+})
+
